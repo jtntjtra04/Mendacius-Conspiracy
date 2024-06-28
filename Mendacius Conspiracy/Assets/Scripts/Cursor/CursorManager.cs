72aecf47b1351293462221a6e294c821a1dcff26
KuroPika04 <justintjitra@gmail.com>
2024-06-28 23:07:57 +0700

    CHANGE CURSOR AND COPIUM WIN

--- Mendacius Conspiracy/Assets/Scripts/Cursor/CursorManager.cs
+++ Mendacius Conspiracy/Assets/Scripts/Cursor/CursorManager.cs
@@ -5,10 +5,16 @@ using UnityEngine;
 public class CursorManager : MonoBehaviour
 {
     public Texture2D default_cursor;
+    public Vector2 cursor_hotspot;
     public Texture2D hover_cursor;
+    public Vector2 hover_cursor_hotspot;
 
-    private void Start()
+    public void OnMouseEnter()
     {
-        Cursor.SetCursor(default_cursor, Vector2.zero, CursorMode.ForceSoftware);
+        Cursor.SetCursor(hover_cursor, hover_cursor_hotspot, CursorMode.Auto);
+    }
+    public void OnMouseExit()
+    {
+        Cursor.SetCursor(default_cursor, cursor_hotspot, CursorMode.Auto);
     }
 }
